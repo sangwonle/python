f83f401bded2dc7852385b7b015b1748b06a75b0
yacro <yacross24@gmail.com>
2022-01-18 16:57:50 +0900

    - 정리

---
+++ module.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+import platform
+
+if platform.system() == 'Darwin': #맥
+        plt.rc('font', family='AppleGothic') 
+elif platform.system() == 'Windows': #윈도우
+        plt.rc('font', family='Malgun Gothic') 
+elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+        plt.rc('font', family='Malgun Gothic') 
+
+plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
+
